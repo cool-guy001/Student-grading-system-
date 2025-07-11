63508dd9c0fb44cca47cc077f575758e96f06603
cool-guy001 <okaforchibuikem111@gmail.com>
2025-07-11 12:56:39 +0100

    Create Student grading system.py
    This is the code(in python)

---
+++ Student grading system.py
@@ -0,0 +1,14 @@
+score = int(input("Enter your score: "))
+
+if score <=100>= 70:
+   print("grade: A")
+elif score <=70>= 60:
+    print("grade: B")
+elif score <=60>= 50:
+    print("grade: C")
+elif score <=50>= 40:
+    print("grade: D")
+elif score <=40>=0:
+    print("grade: F")
+else:
+    print("invaild result")
